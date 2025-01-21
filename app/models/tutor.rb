4be57c1deaccd7789c73c5fbe02ed51d09d96dd8
Eve Waller <evew1991@outlook.com>
2025-01-21 17:13:13 +0000

    corrected tutor controller

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,9 +1,11 @@
 class Tutor < ApplicationRecord
-  belongs_to :subject
   belongs_to :user
+  belongs_to :subject
   has_many :bookings, dependent: :destroy
-  has_many :subjects, dependent: :destroy
-  has_many :users, through: :bookings
-  # has_one_attached :photo
+
+  has_many :tutor_subjects
   has_many :subjects, through: :tutor_subjects
+
+  has_many :bookings
+  has_many :users, through: :bookings
 end
