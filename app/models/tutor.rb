027fee0199e4c0a1546cc84279c8582de95c134b
rob roffey <roffeyre@gmail.com>
2025-01-21 14:53:59 +0000

    x2 controllers added

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,4 +1,8 @@
 class Tutor < ApplicationRecord
   belongs_to :subject
   belongs_to :user
+  has_many :bookings,
+  has_many :subjects, dependent: :destroy
+  has_many :users through: :bookings
+  has_many :subjects, through: :tutor_subjects
 end
