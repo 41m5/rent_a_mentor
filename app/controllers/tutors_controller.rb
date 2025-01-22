51da4e55b6c2998ee6a9ccdca26eec52d53001b3
Eve Waller <evew1991@outlook.com>
2025-01-22 10:06:54 +0000

    added extra feature to new mentor

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,4 +1,6 @@
 class TutorsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @tutors = Tutor.all
   end
@@ -8,13 +10,18 @@ class TutorsController < ApplicationController
   end
 
   def new
-    @tutor = Tutor.new
+    @tutor = Tutor.new(
+      first_name: current_user.first_name,
+      last_name: current_user.last_name,
+      email: current_user.email
+    )
   end
 
   def create
     @tutor = Tutor.new(tutor_params)
+    @tutor.user = current_user
     if @tutor.save
-      redirect_to tutors_path(@tutor)
+      redirect_to tutors_path(@tutor), notice: "You have successfully signed up as a mentor."
     else
       render :new, status: :unprocessable_entity
     end
